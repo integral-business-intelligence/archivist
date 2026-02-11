710c381e5c224dad6846de59abb55edd346410be
anthony-sclafani <anthony.sclafani@outlook.com>
2026-02-10 22:04:00 -0800

    Quartz sync: Feb 10, 2026, 10:03 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "integral-business-intelligence.github.io/archivist",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
